30c3064b6840b99397088d7eebf00798ec93af64
Anca <anca.etodirica@gmail.com>
2015-11-22 00:13:43 +0200

    added function for custom marker

--- app/src/main/java/com/transitiasi/enums/Status.java
+++ app/src/main/java/com/transitiasi/enums/Status.java
@@ -1,10 +1,26 @@
 package com.transitiasi.enums;
 
+import com.transitiasi.R;
+
 /**
  * Created by Anca Todirica on 21-Nov-15.
  */
 public enum Status {
     GREEN, RED, ORANGE;
 
+    public static int getColor(String color) {
+        switch (color) {
+            case "GREEN":
+                return R.color.green;
 
+            case "RED":
+                return R.color.red;
+
+            case "ORANGE":
+                return R.color.orange;
+
+
+        }
+        return R.color.red;
+    }
 }
